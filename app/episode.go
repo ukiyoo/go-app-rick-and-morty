3d74492e6773b75b3fc392f2117b0e24681cd6d7
ukiyoo-home <ukiyoo@example.com>
2021-01-19 01:24:27 +0400

    episode section

--- app/episode.go
+++ app/episode.go
@@ -66,51 +66,55 @@ func (e *Episode) OnMount(ctx app.Context) {
 
 func (e *Episode) Render() app.UI {
 	seasons := api.Seasons()
-	return app.Div().Class("column").Body(
-		app.Div().Class("tabs is-toggle").Body(
-			app.Ul().Body(
-				app.Range(seasons).Slice(func(i int) app.UI {
-					return app.If(seasons[i] == e.activeTab,
-						app.Li().Class("is-active").Style("z-index", "0").Body(
-							app.A().Href("#").OnClick(e.onShowEpisodes).Body(
-								app.Span().Text(seasons[i]),
-							),
-						),
-					).Else(
-						app.Li().Class("").Body(
-							app.A().Href("#").OnClick(e.onShowEpisodes).Body(
-								app.Span().Text(seasons[i]),
-							),
-						),
-					)
-				}),
-			),
-		),
-		app.Div().Class("columns is-multiline").Body(
-			app.Range(e.Response.Results).Slice(func(i int) app.UI {
-				episode := e.Response.Results[i]
-				return app.Div().Class("column is-6").Body(
-					app.A().Href("#").Body(
-						app.Div().Class("box").Body(
-							app.Article().Class("media").Body(
-								app.Div().Class("media-content").Body(
-									app.Div().Class("content").Body(
-										app.P().Body(
-											app.Strong().Text(episode.Name),
-											app.Br(),
-											app.Strong().Text(episode.AirDate),
-											app.Br(),
-											app.Small().Class("has-text-grey-light").Text("Episode: "),
-											app.Br(),
-											app.Text(episode.Episode),
+	return app.Div().Class("section").Body(
+		app.Div().Class("container").Body(
+			app.Div().Class("column").Body(
+				app.Div().Class("tabs is-toggle").Body(
+					app.Ul().Body(
+						app.Range(seasons).Slice(func(i int) app.UI {
+							return app.If(seasons[i] == e.activeTab,
+								app.Li().Class("is-active").Style("z-index", "0").Body(
+									app.A().Href("#").OnClick(e.onShowEpisodes).Body(
+										app.Span().Text(seasons[i]),
+									),
+								),
+							).Else(
+								app.Li().Class("").Body(
+									app.A().Href("#").OnClick(e.onShowEpisodes).Body(
+										app.Span().Text(seasons[i]),
+									),
+								),
+							)
+						}),
+					),
+				),
+				app.Div().Class("columns is-multiline").Body(
+					app.Range(e.Response.Results).Slice(func(i int) app.UI {
+						episode := e.Response.Results[i]
+						return app.Div().Class("column is-6").Body(
+							app.A().Href("#").Body(
+								app.Div().Class("box").Body(
+									app.Article().Class("media").Body(
+										app.Div().Class("media-content").Body(
+											app.Div().Class("content").Body(
+												app.P().Body(
+													app.Strong().Text(episode.Name),
+													app.Br(),
+													app.Strong().Text(episode.AirDate),
+													app.Br(),
+													app.Small().Class("has-text-grey-light").Text("Episode: "),
+													app.Br(),
+													app.Text(episode.Episode),
+												),
+											),
 										),
 									),
 								),
 							),
-						),
-					),
-				)
-			}),
+						)
+					}),
+				),
+			),
 		),
 	)
 }
